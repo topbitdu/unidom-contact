ae84b7da0695486e8461de8b02b5c6da5291084f
Topbit Du <topbit.du@gmail.com>
2017-01-12 21:16:26 +0800

    1, Improve the Contact Subscription model for the document.

--- app/models/unidom/contact/contact_subscription.rb
+++ app/models/unidom/contact/contact_subscription.rb
@@ -17,6 +17,13 @@ class Unidom::Contact::ContactSubscription < Unidom::Contact::ApplicationRecord
   scope :contact_is,    ->(contact)    { where contact:    contact    }
   scope :subscribed_by, ->(subscriber) { where subscriber: subscriber }
 
+  ##
+  # 将联系方式 contact 和订阅者 subscriber 关联起来。
+  # name 是关联的名称，主要用于备注作用，缺省为订阅者的 name 。
+  # primary 是主要联系方式标志，缺省为 true 。
+  # grade 是评分，缺省为 0 。 priority 是优先级，缺省为 0 。
+  # opened_at 是生效时间。
+  # 如： Unidom::Contact::ContactSubscription.subscribe! contact: phone_number, subscriber: selected_person
   def self.subscribe!(contact: nil, subscriber: nil, name: subscriber.try(:name), primary: true, grade: 0, priority: 0, opened_at: Time.now)
     contact_is(contact).subscribed_by(subscriber).valid_at(now: opened_at).alive.first_or_create! name: name, elemental: primary, grade: grade, priority: priority, opened_at: opened_at
   end
