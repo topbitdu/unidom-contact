43b1583581a50af17cc37a9c0760549fb0f887c5
Topbit Du <topbit.du@gmail.com>
2017-01-27 14:38:14 +0800

    1, Models RSpec examples manifest.

---
+++ lib/unidom/contact/models_rspec.rb
@@ -0,0 +1,2 @@
+require 'rspec/models/unidom/contact/contact_subscription_spec' unless Unidom::Common::Neglection.namespace_neglected? 'Unidom::Contact::ContactSubscription'
+require 'rspec/models/unidom/contact/email_address_spec'        unless Unidom::Common::Neglection.namespace_neglected? 'Unidom::Contact::EmailAddress'
