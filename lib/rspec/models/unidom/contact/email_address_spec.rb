eda47fbb20eb2eaf19b1dd7bbd1e08e7594b1ae5
Topbit Du <topbit.du@gmail.com>
2017-02-13 09:45:55 +0800

    1, Improve the Email Address spec for the validations on the #full_address attribute.

--- lib/rspec/models/unidom/contact/email_address_spec.rb
+++ lib/rspec/models/unidom/contact/email_address_spec.rb
@@ -15,6 +15,23 @@ describe Unidom::Contact::EmailAddress, type: :model do
 
     it_behaves_like 'Unidom::Common::Concerns::ModelExtension', model_attributes
 
+    full_address_max_length = described_class.columns_hash['full_address'].limit
+
+    it_behaves_like 'validates', model_attributes, :full_address,
+      {                               } => 0,
+      { full_address: nil             } => 2,
+      { full_address: ''              } => 2,
+      { full_address: 'l'             } => 1,
+      { full_address: 'l@'            } => 1,
+      { full_address: 'l@d'           } => 1,
+      { full_address: 'l@us'          } => 0,
+      { full_address: 'l@com'         } => 0,
+      { full_address: 'l@jobs'        } => 0,
+      { full_address: 'l@jobs.com.cn' } => 0,
+      { full_address: "l@#{'c'*(full_address_max_length-6)}.de" } => 0,
+      { full_address: "l@#{'c'*(full_address_max_length-5)}.de" } => 0,
+      { full_address: "l@#{'c'*(full_address_max_length-4)}.de" } => 1
+
   end
 
 end
