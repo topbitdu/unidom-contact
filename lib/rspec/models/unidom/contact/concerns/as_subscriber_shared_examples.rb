69fb6a626986371cfb61ce036611fc3310aefe56
Topbit Du <topbit.du@gmail.com>
2017-03-20 16:02:45 +0800

    1, As Subscriber shared examples.

---
+++ lib/rspec/models/unidom/contact/concerns/as_subscriber_shared_examples.rb
@@ -0,0 +1,21 @@
+shared_examples 'Unidom::Contact::Concerns::AsSubscriber' do |model_attributes|
+
+  contact_subscription_1_attribtues = {
+      contact_id:   SecureRandom.uuid,
+      contact_type: 'Unidom::Contact::Contact::Mock',
+      name:         'Name #1',
+      grade:        5,
+      priority:     2
+    }
+
+  contact_subscription_2_attribtues = {
+      contact_id:   SecureRandom.uuid,
+      contact_type: 'Unidom::Contact::Contact::Mock',
+      name:         'Name #2',
+      grade:        2,
+      priority:     4
+    }
+
+  it_behaves_like 'has_many', model_attributes, :contact_subscriptions, Unidom::Contact::ContactSubscription, [ contact_subscription_1_attribtues, contact_subscription_2_attribtues ]
+
+end
