2501ef2a84536d9de482539599c269c30a4f023d
Topbit Du <topbit.du@gmail.com>
2017-03-30 21:17:54 +0800

    1, Improve the As Contact shared examples for the coding style.

--- lib/rspec/models/unidom/contact/concerns/as_contact_shared_examples.rb
+++ lib/rspec/models/unidom/contact/concerns/as_contact_shared_examples.rb
@@ -1,21 +1,25 @@
 shared_examples 'Unidom::Contact::Concerns::AsContact' do |model_attributes|
 
-  contact_subscription_1_attribtues = {
-      subscriber_id:   SecureRandom.uuid,
-      subscriber_type: 'Unidom::Contact::Subscriber::Mock',
-      name:            'Name #1',
-      grade:           5,
-      priority:        2
-    }
+  context do
 
-  contact_subscription_2_attribtues = {
-      subscriber_id:   SecureRandom.uuid,
-      subscriber_type: 'Unidom::Contact::Subscriber::Mock',
-      name:            'Name #2',
-      grade:           2,
-      priority:        4
-    }
+    contact_subscription_1_attribtues = {
+        subscriber_id:   SecureRandom.uuid,
+        subscriber_type: 'Unidom::Contact::Subscriber::Mock',
+        name:            'Name #1',
+        grade:           5,
+        priority:        2
+      }
 
-  it_behaves_like 'has_many', model_attributes, :contact_subscriptions, Unidom::Contact::ContactSubscription, [ contact_subscription_1_attribtues, contact_subscription_2_attribtues ]
+    contact_subscription_2_attribtues = {
+        subscriber_id:   SecureRandom.uuid,
+        subscriber_type: 'Unidom::Contact::Subscriber::Mock',
+        name:            'Name #2',
+        grade:           2,
+        priority:        4
+      }
+
+    it_behaves_like 'has_many', model_attributes, :contact_subscriptions, Unidom::Contact::ContactSubscription, [ contact_subscription_1_attribtues, contact_subscription_2_attribtues ]
+
+  end
 
 end
